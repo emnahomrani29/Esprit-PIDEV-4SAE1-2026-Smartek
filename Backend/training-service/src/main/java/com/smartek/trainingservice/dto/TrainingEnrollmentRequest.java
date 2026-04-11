c23c2332559de45fe4ca8e71ef6535b1804a7a65
Wafachabbi <125609969+wafachabbi@users.noreply.github.com>
2026-04-11 11:58:16 +0100

    Delete Backend/training-service directory

--- Backend/training-service/src/main/java/com/smartek/trainingservice/dto/TrainingEnrollmentRequest.java
+++
@@ -1,9 +0,0 @@
-package com.smartek.trainingservice.dto;
-
-import lombok.Data;
-
-@Data
-public class TrainingEnrollmentRequest {
-    private Long trainingId;
-    private Long userId;
-}
--- Backend/training-service/src/main/java/com/smartek/trainingservice/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.smartek.trainingservice.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
